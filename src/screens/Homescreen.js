231ab8497d4b8ecc8e86dab0d7f7ed18d088b21c
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2023-01-05 18:47:59 +0530

    commit

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import React,{ useState } from "react";
+import React,{ useState, useEffect  } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPizzas } from "../actions/pizzaActions";
 import Error from "../components/Error";
