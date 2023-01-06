0bec921d5c7e6317aac33e509dbcfe335fbf4056
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2023-01-06 11:53:18 +0530

    commit

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,7 +1,5 @@
 export const addToCart=(pizza , quantity , varient)=>(dispatch , getState)=>{
 
-
-
     var cartItem = {
         name : pizza.name ,
         _id : pizza._id,
@@ -26,8 +24,7 @@ export const addToCart=(pizza , quantity , varient)=>(dispatch , getState)=>{
             dispatch({type:'ADD_TO_CART' , payload : cartItem})
         }
        
-    }
-    
+    }   
 
     const cartItems = getState().cartReducer.cartItems
     localStorage.setItem('cartItems' , JSON.stringify(cartItems))
@@ -38,12 +35,8 @@ export const addToCart=(pizza , quantity , varient)=>(dispatch , getState)=>{
 
 export const deleteFromCart=(pizza)=>(dispatch , getState)=>{
 
-
      dispatch({type:'DELETE_FROM_CART' , payload:pizza})      
      const cartItems = getState().cartReducer.cartItems
      localStorage.setItem('cartItems' , JSON.stringify(cartItems))
-
-
-  
 
 }
--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -27,7 +27,7 @@ export const getUserOrders=()=>async (dispatch,getState)=>{
   dispatch({type:'GET_USER_ORDERS_REQUEST'})
   
   try {
-      const response = await axios.post('/api/orders/getuserorders' , {userid : currentUser._id})
+      const response = await axios.post(`${config.api}/api/orders/getuserorders` , {userid : currentUser._id})
 
       
       console.log(response);
@@ -45,7 +45,7 @@ export const getAllOrders=()=>async (dispatch,getState)=>{
   dispatch({type:'GET_ALLORDERS_REQUEST'})
   
   try {
-      const response = await axios.get('/api/orders/getallorders')
+      const response = await axios.get(`${config.api}/api/orders/getallorders`)
 
       
       console.log(response);
@@ -62,10 +62,10 @@ export const deliverOrder=(orderid)=>async dispatch=>{
 
 
     try {
-      const response = await axios.post('/api/orders/deliverorder' , {orderid})
+      const response = await axios.post(`${config.api}/api/orders/deliverorder` , {orderid})
       console.log(response);
       alert('Order Delivered')
-      const orders = await axios.get('/api/orders/getallorders')
+      const orders = await axios.get(`${config.api}/api/orders/getallorders`)
       dispatch({type:'GET_ALLORDERS_SUCCESS' , payload:orders.data})
     } catch (error) {
       console.log(error);
